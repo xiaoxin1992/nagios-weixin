becda59aa6b814be614305721cf8554e92b35d99
Your Name <you@example.com>
2016-03-13 01:07:57 +0800

    update

--- wxweb.py
+++ wxweb.py
@@ -3,24 +3,29 @@ from flask import Flask, make_response,request
 import xml.etree.cElementTree as ET
 import hashlib
 import json
-app = Flask(__name__)
 token = "xiaoxin"
 def sha1(data):
 	m = hashlib.sha1()
 	m.update(data.encode('utf-8'))
 	return m.hexdigest()
+def get_ip():
+	path = "./conf/wx_server_ip.txt"
+	with open(path, 'r') as f:
+		data = f.read()
+	return json.loads(data)
+app = Flask(__name__)
 @app.route("/")
 def index():
+	if request.remote_addr.strip() not in get_ip():
+		return "滚犊子!"
 	if request.method == "GET":
 		signature = request.args.get('signature')
 		echostr = request.args.get('echostr')
 		timestamp = request.args.get('timestamp')
 		nonce = request.args.get('nonce')
-		data = [token, timestamp, nonce]
-
-		if None is data:
-			data.sort()
-	if signature:
+		if not echostr:
+			return "滚犊子!"
+		data = sorted([token, timestamp, nonce])
 		if sha1(''.join(data)) == signature.strip():
 			return echostr.strip()
 
